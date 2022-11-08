ddcb1758fd7988df20769d871657ec331f8f660f
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-11-08 18:17:24 -0300

    atlz

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const FRONT = "card_front"
 const BACK = "card_back"
 
-let apps = ['bootstrap',
+let techs = ['bootstrap',
     'css',
     'electron',
     'firebase',
@@ -11,3 +11,61 @@ let apps = ['bootstrap',
     'mongo',
     'node',
     'react'];
+
+
+let cards = null;
+
+startGame();
+
+function startGame() {
+  cards =  createCardsFromTechs(techs);
+  shuffleCards(cards);
+
+  console.log(cards);
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] =  [cards[currentIndex],  cards[randomIndex]];
+    }
+}
+
+
+
+
+
+
+function createCardsFromTechs(techs) {
+
+    let cards = [];
+// isso tudo ta sendo adcionado em tech e em tech ta sendo adcionado cada elemento pelo of de techs, depois tudo vai pra cards
+    for (let tech of techs) {
+        cards.push(createPairFromTech(tech));
+    }
+    
+    return  cards.flatMap(pair => pair)
+}
+
+function createPairFromTech(tech) {
+
+    return [{
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }, {
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }]
+}
+
+function createIdWithTech(tech){
+    return tech + parseInt(Math.random() * 1000);
+}
